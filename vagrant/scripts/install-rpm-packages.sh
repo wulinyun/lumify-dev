320d06b88c0211d5bc25352523cdac2013a136d0
wulinyun <lin.wu@landasoft.com>
2019-04-17 13:45:51 +0800

    放开install-rpm中epel有关的东西，用于erlang依赖环境的安装

--- vagrant/scripts/install-rpm-packages.sh
+++ vagrant/scripts/install-rpm-packages.sh
@@ -5,12 +5,14 @@ curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
 
 #新增部分结束
 
-rpm -Uhv http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+#rpm -Uhv http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 yum update -y
 
 #system tools
 yum install -y wget curl tar sudo openssh-server openssh-clients git nodejs npm libuuid-devel libtool zip unzip rsync which erlang cmake bison
 yum install -y bzip2
+rpm -ivh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+yum install -y erlang
 #ffmpeg
 #yum install -y lumify-videolan-x264 lumify-fdk-aac lumify-lame lumify-opus lumify-ogg lumify-vorbis lumify-vpx lumify-theora lumify-ffmpeg
 
